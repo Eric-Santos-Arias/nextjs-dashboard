fecf18bd8106ddcb040565b17e41d12469165737
Eric <ericsantos502502@gmail.com>
2024-02-29 21:53:58 -0600

    all

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css'
+import {monserrat} from '@/app/ui/fonts'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${monserrat.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
